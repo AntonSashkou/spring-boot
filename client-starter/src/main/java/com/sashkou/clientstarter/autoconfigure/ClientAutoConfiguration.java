4132e4e44dacdbde0ea89eef586fad6c8f2d4a3c
Anton <anton.sashkov@firedome.io>
2023-02-08 18:26:53 +0100

    fix copypaste

--- client-starter/src/main/java/com/sashkou/clientstarter/autoconfigure/ClientAutoConfiguration.java
+++ client-starter/src/main/java/com/sashkou/clientstarter/autoconfigure/ClientAutoConfiguration.java
@@ -1,16 +1,18 @@
 package com.sashkou.clientstarter.autoconfigure;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sashkou.domain.User;
+import com.sashkou.clientstarter.service.UserRestClient;
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import feign.okhttp.OkHttpClient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 @EnableConfigurationProperties({ClientStarterProperties.class})
@@ -19,12 +21,14 @@ public class ClientAutoConfiguration {
     private final ClientStarterProperties properties;
 
     @Bean
-    public User deviceDataDeviceService() {
+    public UserRestClient userRestClient() {
+        log.info("Configuring userRestClient");
+
         return Feign.builder()
             .client(new OkHttpClient())
             .encoder(new JacksonEncoder(objectMapper))
             .decoder(new JacksonDecoder(objectMapper))
             .decode404()
-            .target(User.class, properties.getHost());
+            .target(UserRestClient.class, properties.getHost());
     }
 }
